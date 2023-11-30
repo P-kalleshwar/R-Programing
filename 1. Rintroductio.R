b827de7f94475c6954a7d69376e1323d109c6b0d
P-kalleshwar <152473771+P-kalleshwar@users.noreply.github.com>
2023-11-30 12:22:28 +0530

    Add files via upload

---
+++ 1. Rintroductio.R
@@ -0,0 +1,45 @@
+"hello akash"
+'hello world'
+10
+name1="kalleshwar"
+age=25
+age
+x=21
+x
+name
+num="20"
+age=age+5
+age
+
+45*98-98
+
+text="excellent"
+
+paste("excelR is" ,text)
+paste("my name is",name)
+var=var2=var3="orange"
+
+myvar="kalleshwar"
+my_var="Akash"
+myVar="kalu"
+mYVAR="Rahul"
+myvar3="okay"
+this.year=2045
+
+x=98
+class(x)
+class(name)
+
+#integer-whole+ve- ve l
+y=1000L
+
+#logical/boolean
+
+x=TRUE # or T, FALSE, F
+class(x)
+x=F
+
+3>5
+5<9
+5==5
+1 !=9
